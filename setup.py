d4281c27b0172848e16367ba97ece8612d12e3c0
Muhammad Hamdan <muhammad.hamdan@intel.com>
2022-11-05 20:34:34 -0700

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-VERSION = '1.0.5'
+VERSION = '1.0.6'
 DESCRIPTION = 'Python wrapper for ThreadPoolExecutor to easily multithread resource bound tasks'
 LONG_DESCRIPTION = open('README.md').read()
 # Setting up
