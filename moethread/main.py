7c214a9b34da318e334c061a902f9127da1dc17a
Muhammad Hamdan <muhammad.hamdan@intel.com>
2023-08-29 14:41:52 -0700

    Update unittests

--- moethread/main.py
+++ moethread/main.py
@@ -43,14 +43,14 @@ def progress(count, total, st, return_str=False):
         eta = (100.0 * elapsed_time)/ completed_percent -  elapsed_time
         tmp = elapsed_time/count
         if tmp < 0.001:
-            latency = f"{1000000*tmp:0.2f} us/item"
+            latency = f"{1e6*tmp:0.2f} us/item"
         elif tmp < 1:
-            latency = f"{1000*tmp:0.2f} ms/item"
+            latency = f"{1e3*tmp:0.2f} ms/item"
         else:
             latency = f"{tmp:0.2f} s/item"
         msg = f"\r[ STATUS ] Progress: {completed:0.2%} | Processed: {count}/{total} | " \
               f"Elapsed-time: {elapsed_time:0.2f}s | ETA: {eta:0.3f}s ~ " \
-              f"{round(count/elapsed_time)} items/s @ {latency}"
+              f"{count/elapsed_time:0.1f} items/s @ {latency}"
         if return_str:
             return msg
         else:
