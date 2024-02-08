207a5d2bec95312525742c3f8b03ed1503e35851
mhamdan91 <mhamdan.usa@gmail.com>
2024-02-07 18:55:27 -0800

    Minor changes

--- moethread/__init__.py
+++ moethread/__init__.py
@@ -1,4 +1,4 @@
 from .version import __version__
 from .version import __copyright__
 from .version import __author__
-from .main import parallel_call, progress, mtdo, mtdo_from_csv, mtdo_from_json
+from .main import parallel_call, progress, format_time, format_latency, mtdo, mtdo_from_csv, mtdo_from_json
